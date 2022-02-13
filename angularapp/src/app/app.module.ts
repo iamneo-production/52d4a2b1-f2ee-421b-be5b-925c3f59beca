a1c9f6c26d484181091f587a47574d01ba8b8ccc
srvignesh1998 <vignesh1998srv@gmail.com>
2022-02-13 21:14:34 +0000

    Setup Completed

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -1,30 +1,16 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule, ReactiveFormsModule  } from '@angular/forms';
+import { NgModule } from '@angular/core';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './components/login/login.component';
-import { ForgetPasswordComponent } from './components/forget-password/forget-password.component';
-import { NotFoundComponent } from './components/not-found/not-found.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { SignupComponent } from './components/signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LoginComponent,
-    ForgetPasswordComponent,
-    NotFoundComponent,
-    SignupComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    NgbModule,
-    FontAwesomeModule,
-    FormsModule,
-    ReactiveFormsModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
