7a65bf4fadfba56be0192c89c40e22c9f120c33d
jalarakesh19 <jalarakesh19@gmail.com>
2022-04-04 10:47:48 +0000

    auth added

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -3,25 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './auth/login/login.component';
-import { SignUpComponent, SignupComponent } from './auth/signup/signup.component';
-import { AdminComponent } from './admin/admin.component';
-import { GetuserComponent } from './admin/user-control/getuser/getuser.component';
-import { AdduserComponent } from './admin/user-control/adduser/adduser.component';
-import { EdituserComponent } from './admin/edituser/edituser.component';
-import { AuthComponent } from './auth/auth.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LoginComponent,
-    SignUpComponent,
-    AdminComponent,
-    GetuserComponent,
-    AdduserComponent,
-    EdituserComponent,
-    AuthComponent,
-    SignupComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
